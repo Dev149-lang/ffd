517efbfc21ddfaf6536e9134d6dbb75e70eb7a9d
Dev149-lang <devrajput18vk@gmail.com>
2025-08-02 22:11:52 +0000

    conversion of data files

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,12 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueinnumber = Number(score);
+console.log(typeof valueinnumber)
+
+// "33" ==> 33
+// "33abc" ==> NaN
+// true => 1; false=> 0
+
