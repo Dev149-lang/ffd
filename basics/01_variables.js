a8a6b7592357f47531e3c5774d8e560f20ee1671
Dev149-lang <devrajput18vk@gmail.com>
2025-08-02 10:41:46 +0000

    container config done

---
+++ basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 13343
+let accountEmail = "devrajput18vk@gmail.com"
+var accountPassword = "3455" //dont use var
+accountCity = "dehradun"
+let accountState;
+// accountId = 23 not allowed
+
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21133"
+accountCity = "gaya"
+
+console.log(accountId);
+
+/*
+prefer not to use var becacuse
+of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountState,accountEmail,accountId,accountPassword,])
+
+
