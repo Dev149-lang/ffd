bdc402439c8086e69e7cdadaf3cb346f50222b92
Dev149-lang <devrajput18vk@gmail.com>
2025-08-02 20:40:58 +0000

    container config done

--- basics/02_datatypes.js
+++ basics/02_datatypes.js
@@ -1,1 +1,29 @@
- 5
+  "use strict"; // treat all js code as newer version
+
+ // alert(3+3) // it is not available on nodejs, but is on browser
+
+   console.log(3
+    +
+    3
+   ); // code readability should be high
+
+
+
+
+// number range : 2^53
+// bigint
+// string : ""
+// boolean : true/false
+// null: standalone value
+//undefined ..... let city; here u didnt define city.
+// symbol : unique
+
+// object
+
+
+console.log(typeof "ram");
+console.log(typeof null); // so null is an object
+console.log(typeof undefined); // type is undefined
+
+   
+   
