f9f370e6bb3f48ec6f3ff7915e258dce8db670a8
Dev149-lang <devrajput18vk@gmail.com>
2025-08-03 11:35:33 +0000

    did that

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -1,12 +1,26 @@
-let score = "33"
+// let score = "33"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueinnumber = Number(score);
-console.log(typeof valueinnumber)
+// let valueinnumber = Number(score);
+// console.log(typeof valueinnumber)
 
 // "33" ==> 33
 // "33abc" ==> NaN
 // true => 1; false=> 0
 
+// ******************** operations ****************
+
+// let value = 33
+// let negValue= -value
+
+// console.log(negValue);
+
+// console.log(2/0);
+console.log(+true);
+console.log(+"");
+
+
+
+ 
