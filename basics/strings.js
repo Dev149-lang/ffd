1b8a506e9569a015b85062dd17e98b055588a345
Dev149-lang <devrajput18vk@gmail.com>
2025-08-04 12:24:33 +0000

    ffg

--- basics/strings.js
+++ basics/strings.js
@@ -1,10 +1,28 @@
-const uyt = "devvvv"
-const count = 67
+// const uyt = "devvvv"
+// const count = 67
 
-// console.log(uyt + count + "   value" );
+// // console.log(uyt + count + "   value" );
 
-console.log(`hello my name is ${uyt} my num is ${count} `);
+// console.log(`hello my name is ${uyt} my num is ${count} `);
 
 
 
-// 12333jckjkhl vfghkhdjj
+// 12333jckjkhl vfghkhdjj jhvhjj 5945huffdjdf njj 
+
+
+const gamename = new String('hitutrinot');
+
+// console.log(gamename[0]);
+// console.log(gamename.__proto__);
+
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(2));
+
+const anotherstring = gamename.slice(-2,7)
+console.log(anotherstring);
+
+
+
+
+
