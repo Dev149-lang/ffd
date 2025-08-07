1d073104f4a109fb027d5ff898a085a198463ea7
Dev149-lang <devrajput18vk@gmail.com>
2025-08-07 12:04:06 +0000

    ij

--- basics2/objects2.js
+++ basics2/objects2.js
@@ -6,17 +6,32 @@ tinderuser.id = "123adb"
 tinderuser.name = "dev"
 tinderuser.isloggedin = false
 
-console.log(tinderuser);
+console.log(tinderuser.id);
 
 const anotheruser = {
     name: {
         fullname:{
             firstname: "dev",
-            lastname: "kish"
+            lastname: "kish" 
         }
     }
 }
 
 console.log(anotheruser.name.fullname.firstname);
 // console.log(anotheruser.name.fullname?.firstname); ? is for special case when maybe full name is not mentioned
+console.log(tinderuser.hasOwnProperty('isloggedin'));
 
+
+// +***************************
+
+
+
+const course = {
+
+
+}
+
+
+//  react  
+
+const navbar = ({company}) =>
