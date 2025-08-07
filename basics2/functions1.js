53748ce8f4a93ebde40953ac741db0fb0f51075d
Dev149-lang <devrajput18vk@gmail.com>
2025-08-07 16:54:53 +0000

    f

---
+++ basics2/functions1.js
@@ -0,0 +1,49 @@
+// console.log("h");
+// console.log("a");
+// console.log("g");
+
+
+
+// function myname() {
+// console.log("h");
+// console.log("a");
+// console.log("g");
+
+// }
+
+// myname  // refernce without (),doesnt call
+
+// myname()  // execution
+
+
+
+// function add(number1,number2){
+
+//     // console.log(number1+number2); wrong
+
+//     // let result = number1 + number2
+//     // return result;
+// return number1+ number2;
+    
+    
+// }
+
+// console.log(add(4,5));
+
+
+function loggedin(username){
+    if(username === undefined   ){
+            console.log("pls enter");
+            return;
+        }
+ return `${username} is logged in`
+}
+
+console.log(loggedin());
+
+
+
+
+
+
+
