3dee9898be7560213d2445d02992901f652545b2
Dev149-lang <devrajput18vk@gmail.com>
2025-08-09 19:55:30 +0000

    ggh

---
+++ basics2/arrow1.js
@@ -0,0 +1,42 @@
+  const user =  {
+    name: "dev",
+    arm: "uu",
+    age: 21 ,
+
+    wlcomemessage : function() {
+        console.log(`${this.name}, welcome to the site`);
+        console.log(this);
+    }
+  }
+
+// // // console.log(this.name);
+
+// // user.wlcomemessage();
+// // user.name= "rem"
+// // user.wlcomemessage()
+
+// // console.log(this);
+
+// function chai(){
+//   console.log(this);
+  
+// }
+
+// chai()
+
+
+
+// .....nn        
+
+
+// aroow functio
+
+const rai= () => {
+let name = "dev"
+console.log(this)
+
+}
+
+rai()
+
+const nai = 
