ffb9a1ed860d4025881e3d6a695e9eedce280e27
Dev149-lang <devrajput18vk@gmail.com>
2025-08-09 18:39:05 +0000

    ff

---
+++ basics2/scopes1.js
@@ -0,0 +1,14 @@
+ function haram(num){
+    return num*2;
+ }
+
+//  console.log(haram(3)); can declare this before or after
+ 
+//   console.log(addtwo(4));  cant write this before if u declared func using variable 
+
+ const addtwo = function(num){
+    return num+3;
+ }
+
+
+ 
