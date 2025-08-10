30de04c3303405b98090337000d4b653ea5d86ff
Dev Singh <devrajput18vk@gmail.com>
2025-08-10 17:29:50 +0000

    c

---
+++ basics2/one.js
@@ -0,0 +1,42 @@
+// //  === checks whether both things are same data types, 
+// // which == doesnt check
+
+// if(2!="2") {
+//     console.log("executed");
+    
+// }
+// // !== checks both the iequality of value and type, 
+// // != only checks value
+
+
+const month = 4
+
+switch(month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("mar");
+       
+    case 4:
+        console.log("april");
+    case 5:
+        console.log("april");
+        break;
+    case 6:
+        console.log("april");
+       
+
+    default:
+        break;
+        
+}
+
+
+
+// nullish coalescing operator
+
+let val1;
